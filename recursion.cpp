441a1943417718f547bda8d65caedc69a1505eac
Aakash dubey <akashdubey6782@gmail.com>
2026-08-29 22:31:53 +0530

    solving recursion question

--- recursion.cpp
+++ recursion.cpp
@@ -327,3 +327,37 @@ printsubset(arr,i+1,ans[]);}
 //     ps(nums,ans,vec,idx);
 
 // }
+
+//function for nqueens question
+// bool issafe(vector<string> board,int row,int n,int col){
+//         for(int i=0;i<n;i++){
+//             if(board[row][i]=='Q'){
+//                 return false;
+//             }}
+//         for(int i=0;i<n;i++){
+//             if(board[i][col]=='Q'){
+//                 return false;
+//             }}
+//         for(int i=row,j=col;i>=0&&j>=0;i--,j--){
+//             if(board[i][j]=='Q'){
+//                 return false;
+//             }}
+//         for(int i=row,j=col;i>=0&&j<n;i--,j++){
+//             if(board[i][j]=='Q'){
+//                 return false;
+//             }}
+//             return true;
+//         }
+//     void nqueens(vector<string> board , vector<vector<string>>& ans,int row, int n){
+//         if(row==n){
+//             ans.push_back({board});
+//             return;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(issafe(board,row,n,i)){
+//                 board[row][i]='Q';
+//                 nqueens(board,ans,row+1,n);
+//                 board[row][i]='.';
+//             }
+//         }
+//     }  
