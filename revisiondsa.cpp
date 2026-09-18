b3040adfeb6db767c53084a128334af0c792dc46
Aakash dubey <akashdubey6782@gmail.com>
2026-09-18 22:47:23 +0530

    solving paisum

--- revisiondsa.cpp
+++ revisiondsa.cpp
@@ -141,3 +141,183 @@
 // return 0;
 // }
 
+//code for reverse an array
+// #include<iostream>
+// using namespace std;
+// int reversearray(int arr[],int n){
+//     int st=0;
+//     int end=n-1;
+//     while(st<end){
+//         swap(arr[st],arr[end]);
+//         st++;
+//         end--;
+//     }
+// }
+// int main(){
+//     int arr[]={1,2,3,4,5};
+//     int n=5;
+//     reversearray(arr,n);
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// return 0;
+// }
+
+//single number 
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// int singlenumber(vector<int> vec){
+//     int ans =0;
+//     for(int i=0;i<vec.size();i++){
+//         ans=ans^vec[i];
+//     }
+//     return ans;
+// }
+// int main(){
+//     vector<int> vec={1,2,1,2,4};
+//     cout<<singlenumber(vec);
+// return 0;
+// }
+
+//print all subarray
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int subarray(vector<int> vec){
+//     for(int i=0;i<=vec.size();i++){
+//         for(int j=i;j<=vec.size();j++){
+//             for(int k=i;k<j;k++){
+//             cout<<vec[k]<<" "; 
+//         }
+//         cout<<endl;
+//     }}
+// }
+// int main(){
+//     vector<int> vec={1,2,3,4};
+//     subarray(vec);
+// return 0;
+// }
+
+//max subarray sum(brute force approach)
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// int subarray(vector<int> vec){
+//     int maxx=INT8_MIN;
+//     for(int i=0;i<=vec.size();i++){
+//         for(int j=i;j<=vec.size();j++){
+//             int sum=0;
+//             for(int k=i;k<j;k++){
+//             sum=sum+vec[k];
+//             maxx=max(maxx,sum);
+//         }
+//         cout<<endl;
+//     }}
+//     return maxx;
+// }
+// int main(){
+//     vector<int> vec={1,2,3,4};
+//     cout<<subarray(vec);
+// return 0;
+// }
+
+//max subarraysum (optimized approach) //kadanne algorithm
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int subarray(vector<int> vec){
+//     int sum=0;
+//     int maxsum=INT8_MIN;
+//     for(int i=0;i<vec.size();i++){
+//         sum=sum+vec[i];
+//         maxsum= max(sum,maxsum);
+//         if(sum<0){
+//             sum=0;
+//         }
+//     }
+      
+//     return maxsum;
+// }
+// int main(){
+//     vector<int> vec={-1,2,3,-10};
+//     cout<<subarray(vec);
+// return 0;
+// }
+
+//pair sum (brute force)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void pairsum(vector<int> vec,int target){
+//     int n= vec.size();
+    // if(target==vec[0]){
+    //     cout<<vec[0];
+    //     return;
+    // }
+//      int i=0;
+//     if(vec[0]>target){
+//         i=1;
+//     }
+//     for(;i<n;i++){
+//         for(int j=i;j<n;j++){
+//             int sum=0;
+//             for(int k=i;k<n;k++){
+//                 sum+= vec[k];
+//                 if(sum==target){
+//                     cout<<vec[k-1]<<","<<vec[k];
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+// }
+// int main(){
+//     vector<int> vec={12,4,3,8};
+//     int target =11;
+//     pairsum(vec,target);
+// return 0;
+// }
+
+//pairsum (optimized approach)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector<int>  pairsum(vector<int> vec,int target,vector<int> &v){
+//     int st=0,end =vec.size()-1;
+//     if(target==vec[0]){
+//         v.push_back(vec[0]);
+//         return v;
+//     }
+//     if(vec[0]>target){
+//         st=1;
+//     }
+//     while(st<end){
+//         int sum=vec[st]+vec[end];
+//         if(sum>target){
+//             end--;
+//         }
+//         if(sum<target){
+//             st++;
+//         }
+//         if(sum==target){
+//             v.push_back(vec[st]);
+//             v.push_back(vec[end]);
+//             return {v};
+//         }
+//     }
+//     v.push_back(-1);
+//     return v;
+// }
+// int main(){
+//     vector<int> vec={7,4,7,2,8};
+//     int target =6;
+//     vector<int> v;
+//     pairsum(vec,target,v);
+//     for(int val:v){
+//         cout<<val<<" ";
+//     }
+//     return 0;
+// }
